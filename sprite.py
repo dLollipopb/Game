d7557f14a7acd55823b8e091991ae6989b71594f
dLollipopb <nullfl8@gmail.com>
2022-11-21 00:51:40 +0700

    First commit

---
+++ sprite.py
@@ -0,0 +1,7 @@
+import pygame
+
+class Sprite:
+	def __init__(self):
+		self.x=0.0
+		self.y=0.0
+		self.texture=None
--- window.py
+++ window.py
@@ -1,15 +1,25 @@
 import pygame,sys
 
 class Window:
-	def __init__(self,w,h):
-		self.w=w
-		self.h=h
+	def __init__(self,settings):
+		self.w=settings['width']
+		self.h=settings['height']
 		self.flags=0
-		self.scr=None
-	def resizable(self,t):
-		if t:
+		if settings["resizable"]:
 			self.flags|=pygame.RESIZABLE
-		else::
-			self.flags-=pygame.RESIZABLE
-	def create(self):
-		self.scr=pygame.display.set_mode((w,h),self.flags)
+		self.scr=pygame.display.set_mode((self.w,self.h),self.flags)
+		self.shouldclose=False
+		self.drawing|=set()
+	def draw(self,obj):
+		self.drawing|={obj}
+	def undraw(self,obj):
+		self.drawing-={obj}
+	def close(self):
+		self.shouldclose=True
+	def loop(self):
+		while not self.shouldclose:
+			for i in pygame.event.get():
+				if i.type==pygame.QUIT:
+					self.close()
+			pygame.display.flip()
+		pygame.quit()
